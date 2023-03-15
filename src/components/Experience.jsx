ad75648e6ea90cf61fcbc8be4446541d5aebd612
Rutvik2699 <rutvikbhoj2611@gmail.com>
2023-03-15 10:33:39 -0400

    bug fixing

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -77,7 +77,7 @@ const Experience = () => {
                         key={id} 
                         className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
                         <img src={src} alt="" className='w-20 mx-auto'/>
-                        <p className="mt-4">HTML</p>
+                        <p className="mt-4">{title}</p>
                         </div>
                     ))
                 }
