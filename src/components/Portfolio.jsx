1ad80282829ef77ff39925a72d85c9ec521afb1a
Rutvik2699 <rutvikbhoj2611@gmail.com>
2023-03-15 11:11:07 -0400

    a

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -67,8 +67,8 @@ const Portfolio = () => {
                     alt="" 
                     className='rounded-md duration-200 hover:scale-105'/>
                     <div className='flex items-center justify-center'>
-                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
-                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>{demo}</button>
+                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>{code}</button>
                     </div>
                 </div>
                     ))}
