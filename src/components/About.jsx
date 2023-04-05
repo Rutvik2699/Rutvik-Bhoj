8ff017b1c1dc6594dbe104cc6d6a2ee8d4b51fc0
Rutvik2699 <rutvikbhoj2611@gmail.com>
2023-04-05 19:39:47 -0400

    New file

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,11 +11,14 @@ const About = () => {
             </div>
         
         <p className='text-xl mt-20'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+        Hello, I am Rutvik Bhoj, Web Developer with 3 years of experience. I have experience in both Front-end and Back-end development and handle all the aspects of web development.
+        I have good knowledge of programming languages such as HTML, CSS, JavaScript using which I have developed visually appealing websites. I am also proficient in frameworks such as REACT, Vue and have built robust full stack websites.
         </p>
         <br/>
         <p className='text-xl'>
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        Additionally, I have experience in server side scripting languages such as JavaScript, Java and PHP along with databases such as MySQL and MongoDB in which I am capable of building queries and optimizing performance.
+        As as full stack developer I am also comfortable with version control system such as Git and have experience with API to integrate third party services. 
+        I have also worked as a Test engineer which gave me enough skills to test a whole website using automation tools like Selenium and Cypress.
         </p>
     </div>
     </div>
